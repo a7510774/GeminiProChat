996e4b13339ec9a17c322c10d38958f7baddc8ac
chengw <260323117@qq.com>
2024-04-29 16:05:13 +0800

    Update openAI.ts

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@fuyun/generative-ai";
 
 const apiKey = (import.meta.env.GEMINI_API_KEY)
 const apiBaseUrl = (import.meta.env.API_BASE_URL)?.trim().replace(/\/$/, '')
